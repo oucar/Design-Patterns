023654ac85b273b155ae3d14cc95e1c9ee9f035e
oucar <hello@ucaronur.com>
2023-01-16 15:57:43 -0500

    done with the strategy pattern

---
+++ Strategy/Strategy-Pattern/Ducks/ModelDuck.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Strategy_Pattern.Behaviors;
+using Strategy_Pattern.Ducks.Abstractions;
+
+namespace Strategy_Pattern.Ducks
+{
+    public class ModelDuck : Duck
+    {
+
+        public ModelDuck()
+        {
+            FlyBehavior = new FlyNoWay();
+            QuackBehavior = new MuteQuack();
+        }
+
+        public override void Display()
+        {
+            Console.WriteLine("I'm a model duck");
+        }
+    }
+}
---
+++ Strategy/Strategy-Pattern/Ducks/RubberDuck.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Strategy_Pattern.Behaviors;
+using Strategy_Pattern.Ducks.Abstractions;
+
+namespace Strategy_Pattern.Ducks
+{
+    public class RubberDuck : Duck
+    {
+        public RubberDuck()
+        {
+            FlyBehavior = new FlyNoWay();
+            QuackBehavior = new Squeak();
+        }
+
+        public override void Display()
+        {
+            Console.WriteLine("I'm a rubber duckie");
+        }
+    }
+}
