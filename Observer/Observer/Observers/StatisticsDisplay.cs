1b7e3cd3e108d8e58ef8975252e7b306e7eb2f5c
Onur Ucar <onurucar@iu.edu>
2023-01-27 12:13:58 -0500

    done with the observer pattern

---
+++ Observer/Observer/Observers/StatisticsDisplay.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Observer.Observers.Abstractions;
+using Observer.Subjects.Abstractions;
+
+namespace WeatherStation.Observers
+{
+    public class StatisticsDisplay : IObserver, IDisplayElement
+    {
+        private double _maxTemp;
+        private double _minTemp = 200;
+        private double _tempSum;
+        private int _numReadings;
+        private readonly ISubject _weatherData;
+
+        public StatisticsDisplay(ISubject weatherData)
+        {
+            _weatherData = weatherData;
+            _weatherData.RegisterObserver(this);
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Avg/Max/Min temperature = " + (_tempSum / _numReadings)
+                                                           + "/" + _maxTemp + "/" + _minTemp);
+        }
+
+        public void Update(double temp, double humidity, double pressure)
+        {
+            _tempSum += temp;
+            _numReadings++;
+
+            if (temp > _maxTemp)
+            {
+                _maxTemp = temp;
+            }
+
+            if (temp < _minTemp)
+            {
+                _minTemp = temp;
+            }
+
+            Display();
+        }
+    }
+}
--- Observer/Observer/Program.cs
+++ Observer/Observer/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Observer.Observers;
+using Observer.Subjects;
+using WeatherStation.Observers;
+
+
+namespace Observer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            WeatherData weatherData = new WeatherData();
+
+            CurrentConditionsDisplay currentDisplay = new CurrentConditionsDisplay(weatherData);
+            StatisticsDisplay statisticsDisplay = new StatisticsDisplay(weatherData);
+            ForecastDisplay forecastDisplay = new ForecastDisplay(weatherData);
+            HeatIndexDisplay heatIndexDisplay = new HeatIndexDisplay(weatherData);
+
+            weatherData.SetMeasurements(80, 65, 30.4d);
+            weatherData.SetMeasurements(82, 70, 29.2d);
+            weatherData.SetMeasurements(78, 90, 29.2d);
+        }
+    }
+}
