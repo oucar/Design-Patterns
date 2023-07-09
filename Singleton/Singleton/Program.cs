5dcce11bcfacdfbe32b5fbbe72199b8989aa1d3e
oucar <hello@ucaronur.com>
2023-07-09 03:51:09 -0400

    singleton pattern implementation

--- Singleton/Singleton/Program.cs
+++ Singleton/Singleton/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Singleton
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ChocolateBoiler boiler = ChocolateBoiler.Instance;
+            boiler.Fill();
+            boiler.Boil();
+            boiler.Drain();
+
+            // will return the existing instance
+            ChocolateBoiler boiler2 = ChocolateBoiler.Instance;
+        }
+    }
+}
